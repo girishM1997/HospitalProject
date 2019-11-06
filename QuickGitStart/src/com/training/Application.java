ad498774615dbd67a87b2337562c25e988d56bbf
SagarikaBalaraj <geethabalaraj26@gmail.com>
2019-11-06 12:49:37 +0530

    My first commit

---
+++ QuickGitStart/src/com/training/Application.java
@@ -0,0 +1,14 @@
+package com.training;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		List<String> list=Arrays.asList("sagarika","mounika");
+		list.forEach(System.out::println);
+	}
+
+}
